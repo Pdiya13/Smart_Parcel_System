6b5076e63eceadf414b8fd5c30ecf697b409dc1a
Eva Raste <rasteeva3003@gmail.com>
2025-08-03 19:53:01 +0530

    check index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const app = express();
 
-hi 
-hiiii
-helloo
+// Middleware to log every request
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
+});
+
+// Simple test route
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
